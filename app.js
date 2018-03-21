6e3dc67768ba9626537d79a1d3dc42fb72215473
Admin <arlu@umich.edu>
2018-03-21 17:22:53 -0400

    Add UI

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-    const num = req.query.num;
+app.get('/gen', (req, res) => {
+
+    const dnum = req.query.dnum;
+    const fname = req.query.fname;
     const { spawn } = require('child_process');
-    const pyProg = spawn('python',['./mixer.py', num]);
+    const pyProg = spawn('python',['./mixer.py', dnum]);
+    var str = "";
 
-    pyProg.stdout.on('data', function(data) {
-
-        // console.log(data.toString());
-        res.write(data);
-        // res.end('end');
-
+    pyProg.stdout.on('data', (data) => {
+        str += data.toString();
     });
-})
+
+    pyProg.on('close', (code) => {
+        res.set({"Content-Disposition":`attachment; filename=${fname}.json`});
+        res.send(str);
+    });
+    
+});
 
 app.listen(4000, () => console.log('app listening on port 4000!'))
